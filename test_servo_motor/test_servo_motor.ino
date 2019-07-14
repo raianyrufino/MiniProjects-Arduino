f947bd50cd4f2d10df0d0bad45b138db55f40c53
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-07-14 13:50:34 -0300

    test for servo motor operation

---
+++ test_servo_motor/test_servo_motor.ino
@@ -0,0 +1,19 @@
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+int val = 0;
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  
+  myservo.write(180);                  // sets the servo position according to the scaled value
+  delay(800);   
+   myservo.write(0);                  // sets the servo position according to the scaled value
+  delay(800);   
+
+}
+
